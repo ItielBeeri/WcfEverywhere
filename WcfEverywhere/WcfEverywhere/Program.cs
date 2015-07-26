6ef7f4440c219d2d6b0606963133ff8ca74013d2
Asher <asher.barak@gmail.com>
2015-07-26 10:35:57 +0300

    initial

---
+++ WcfEverywhere/WcfEverywhere/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfEverywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This project is a proof of concept for WCF communications using a single service contract and a single service implementation in multiple scenarios, across multiple boundaries, using multiple configurations.");
+            Console.WriteLine("Check out the code, and the ReadMe file and visit https://github.com/AsherBarak/WcfEverywhere.git");
+            Console.ReadKey();
+        }
+    }
+}
