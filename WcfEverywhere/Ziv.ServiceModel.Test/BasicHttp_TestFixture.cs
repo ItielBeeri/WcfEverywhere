d1bf25d8b7f2d8495de95d029bf4eb1dedb69ad7
Nomi1 <Nomy@ziv.co.il>
2015-07-30 16:35:41 +0300

    Add TODO comment

--- WcfEverywhere/Ziv.ServiceModel.Test/BasicHttp_TestFixture.cs
+++ WcfEverywhere/Ziv.ServiceModel.Test/BasicHttp_TestFixture.cs
@@ -15,9 +15,11 @@ namespace Ziv.ServiceModel.Test
         [TestMethod]
         public void TestMethod1()
         {
+            //TODO: Configure DI container
             var baseAddresses = new Uri("http://localhost:8180/");
             using (var serviceHost = new ServiceHost(typeof(DoSomethingService), baseAddresses))
             {
+                //TODO: Configure service host with dependency injecction  
                 serviceHost.Open();
                 using (var client = new DoSomethingClient())
                 {
