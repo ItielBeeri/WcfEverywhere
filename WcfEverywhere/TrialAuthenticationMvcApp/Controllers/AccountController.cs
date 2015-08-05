cfbee36157c729caff12f698f930b68c130af694
ItielBeeri <itiel.bar@gmail.com>
2015-08-05 19:14:13 +0300

    Add mvc for trials

---
+++ WcfEverywhere/TrialAuthenticationMvcApp/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using TrialAuthenticationMvcApp.Models;
+
+namespace TrialAuthenticationMvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginVM login, string redirectUrl)
+        {
+            if (BackServiceProxy.ForwardLoginRequestToService(login))
+            {
+                FormsAuthentication.SetAuthCookie(login.Username, false);
+                return Redirect(redirectUrl);
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login failed");
+                return View();
+            }
+        }
+    }
+}
